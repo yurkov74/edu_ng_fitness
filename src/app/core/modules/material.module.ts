b51313287ec2883881e7907854b836c944c633cb
Iuri Covaliov <iuri.covaliov@meta.md>
2020-09-26 13:45:51 +0200

    27. Working on The Signup Form

--- src/app/core/modules/material.module.ts
+++ src/app/core/modules/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
     exports: [
         MatButtonModule,
+        MatFormFieldModule,
         MatIconModule,
     ]
 })
