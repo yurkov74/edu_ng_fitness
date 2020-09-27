c96e4ac20491356d887623efe693a352f791866c
Iuri Covaliov <iuri.covaliov@meta.md>
2020-09-27 11:31:17 +0200

    34. Adding a Checkbox

--- src/app/core/modules/material.module.ts
+++ src/app/core/modules/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -10,6 +11,7 @@ import {MatInputModule} from '@angular/material/input';
 @NgModule({
     exports: [
         MatButtonModule,
+        MatCheckboxModule,
         MatDatepickerModule,
         MatFormFieldModule,
         MatIconModule,
