203706821ad2dce60675a9c0f39611fcf4913e99
Iuri Covaliov <iuri.covaliov@meta.md>
2020-09-28 21:15:47 +0200

    46. Adding a Tabs Component

--- src/app/core/modules/material.module.ts
+++ src/app/core/modules/material.module.ts
@@ -9,6 +9,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
@@ -22,6 +23,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
         MatListModule,
         MatNativeDateModule,
         MatSidenavModule,
+        MatTabsModule,
         MatToolbarModule,
     ]
 })
