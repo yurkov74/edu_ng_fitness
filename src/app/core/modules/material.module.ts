6ce79ff192c41d3afc8cc29bbee705bb1ebf430c
Iuri Covaliov <iuri.covaliov@meta.md>
2020-09-28 21:29:20 +0200

    47. Adding some "Cards"

--- src/app/core/modules/material.module.ts
+++ src/app/core/modules/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
@@ -15,6 +16,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
     exports: [
         MatButtonModule,
+        MatCardModule,
         MatCheckboxModule,
         MatDatepickerModule,
         MatFormFieldModule,
